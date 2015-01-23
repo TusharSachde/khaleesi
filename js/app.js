ca57c38e4c5c182447adf8b16534778878909c3d
jagruti181 <patiljagruti181@gmail.com>
2015-01-23 14:26:52 +0530

    validate

--- js/app.js
+++ js/app.js
@@ -159,3 +159,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'firebaseservices'])
         templateUrl: "templates/chat1.html"
     }
 });
+var formvalidation = function(allvalidation) {
+            var isvalid2 = true;
+            for (var i = 0; i < allvalidation.length; i++) {
+                console.log("checking");
+                console.log(allvalidation[i].field);
+                if (allvalidation[i].field == "" || !allvalidation[i].field) {
+                    allvalidation[i].validation = "ng-dirty";
+                    isvalid2 = false;
+                }
+            }
+            return isvalid2;
+        };
