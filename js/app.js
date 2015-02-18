48b07ebd307ac2fdc7eadfe9fecc70e7734f357b
Tushar <tushar@wohlig.com>
2015-02-18 12:40:40 +0530

    Some Git

--- js/app.js
+++ js/app.js
@@ -8,21 +8,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'firebaseservices'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
-        cordova.plugins.Keyboard.shrinkView(true);
+        //        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        //        cordova.plugins.Keyboard.disableScroll(true);
+        //        cordova.plugins.Keyboard.shrinkView(true);
         cordova.plugins.Keyboard.overlaysWebView(false);
 
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
-            cordova.plugins.Keyboard.shrinkView(true);
         }
         if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
         }
     });
 })
